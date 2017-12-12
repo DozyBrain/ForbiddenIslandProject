2079ac6ffe8a1f0be678fa6a649f1cb798d31b7b
Nans <Nans@Nans-PC>
2017-12-12 11:21:23 +0100

    creation grille début

--- ProjectForbiddenIsland/src/model/game/TileName.java
+++ ProjectForbiddenIsland/src/model/game/TileName.java
@@ -1,5 +1,36 @@
 package model.game;
 
 public enum TileName {
+    CAVE_OF_EMBERS("Cave of Embers"),
+    OBSERVATORY("Observatory"),
+    SILVER_GATE("Silver Gate"),
+    IRON_GATE("Iron Gate"),
+    PHANTOM_ROCK("Phantom Rock"),
+    TEMPLE_OF_THE_SUN("Temple of the Sun"),
+    WATCHTOWER("Watchtower"),
+    CORAL_PALACE("Coral Palace"),
+    WHISPERING_GARDEN("Whispering Garden"),
+    CRIMSON_FOREST("Crimson Forest"),
+    FOOLS_LANDING("Fools' Landing"),
+    DUNES_OF_DECEPTION("Dunes of Deception"),
+    GOLD_GATE("Gold Gate"),
+    COPPER_GATE("Copper Gate"),
+    LOST_LAGOON("Lost Lagoon"),
+    CAVE_OF_SHADOWS("Cave of Shadows"),
+    TIDAL_PALACE("Tidal Palace"),
+    MISTY_MARSH("Misty Marsh"),
+    TWILIGHT_HOLLOW("Twilight Hollow"),
+    TEMPLE_OF_THE_MOON("Temple of the Moon"),
+    BRONZE_GATE("Bronze Gate"),
+    BREAKERS_BRIDGE("Breakers Bridge"),
+    CLIFFS_OF_ABANDON("Cliffs of Abandon"),
+    HOWLING_GARDEN("Howling Garden");
+    
+    private String name;
+
+    private TileName(String name) {
+        this.name = name;
+    }
+    
     
 }
